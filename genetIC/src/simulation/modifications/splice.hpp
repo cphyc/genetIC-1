fc2a9ca40d0f280586ac5079c3b96c1e14e46eb7
Corentin Cadiou <corentin.cadiou@cphyc.me>
2025-11-04 18:32:59 +0100

    Use Mbar C^-1 Mbar operators

--- genetIC/src/simulation/modifications/splice.hpp
+++ genetIC/src/simulation/modifications/splice.hpp
@@ -33,89 +33,57 @@ namespace modifications {
     return mask;
   }
 
-  // //! Return the field f which satisfies f = a in flagged region while minimising (f-b).C^-1.(f-b) elsewhere
-  // template<typename DataType, typename T=tools::datatypes::strip_complex<DataType>>
-  // fields::Field<DataType,T> spliceOneLevel(fields::Field<DataType,T> & a,
-  //                                          fields::Field<DataType,T> & b,
-  //                                          const fields::Field<DataType,T> & cov) {
+  template<typename DataType, typename T=tools::datatypes::strip_complex<DataType>>
+  fields::OutputField<DataType> Mbar_Cm1_Mbar(
+    const fields::OutputField<DataType> & inputs,
+    const auto& covs, const auto& masks, const auto& masksCompl, size_t Nlevel
+  ) {
+    auto outputs = inputs;
+    // outputs = T_op_T(outputs, [&](const int level, fields::Field<DataType,T> & input) {
+    for (size_t level = 0; level < Nlevel; ++level) {
+      auto & input = outputs.getFieldForLevel(level);
+      input.toFourier();
+      input.applyTransferFunction(covs[level], 0.5);
+      input.toReal();
+      input *= masksCompl[level];
+      input.toFourier();
+      input.applyTransferFunction(covs[level], -1.0);
+      input.toReal();
+      input *= masksCompl[level];
+      input.toFourier();
+      input.applyTransferFunction(covs[level], 0.5);
+      input.toReal();
+    }
+    // });
+    outputs.toReal();
+    return outputs;
+  }
 
-  //     // To understand the implementation below, first read Appendix A of Cadiou et al (2021),
-  //     // and/or look at the 1D toy implementation (in tools/toy_implementation/gene_splicing.ipynb) which
-  //     // contains a similar derivation and near-identical implementation.
-
-  //     assert (&a.getGrid() == &b.getGrid());
-  //     assert (&a.getGrid() == &cov.getGrid());
-  //     auto mask = generateMaskFromFlags(a.getGrid());
-  //     auto maskCompl = generateMaskComplementFromFlags(a.getGrid());
-
-  //     a.toFourier();
-  //     b.toFourier();
-
-  //     // The preconditioner should be almost equal to the covariance.
-  //     // We however set the fundamental of the power spectrum to a non-null value,
-  //     // otherwise, the mean value in the spliced region is unconstrained.
-  //     fields::Field<DataType,T> preconditioner(cov);
-  //     preconditioner.setFourierCoefficient(0, 0, 0, 1);
-
-  //     fields::Field<DataType,T> delta_diff = b-a;
-  //     delta_diff.applyTransferFunction(preconditioner, 0.5);
-  //     delta_diff.toReal();
-
-
-  //     fields::Field<DataType,T> z(delta_diff);
-  //     z*=mask;
-  //     z.toFourier();
-  //     z.applyTransferFunction(preconditioner, -1.0);
-  //     z.toReal();
-  //     z*=maskCompl;
-  //     z.toFourier();
-  //     z.applyTransferFunction(preconditioner, 0.5);
-  //     z.toReal();
-
-  //     auto X = [&preconditioner, &maskCompl](const fields::Field<DataType,T> & input) -> fields::Field<DataType,T>
-  //     {
-  //       fields::Field<DataType,T> v(input);
-  //       assert (!input.isFourier());
-  //       assert (!v.isFourier());
-  //       v.toFourier();
-  //       v.applyTransferFunction(preconditioner, 0.5);
-  //       v.toReal();
-  //       v*=maskCompl;
-  //       v.toFourier();
-  //       v.applyTransferFunction(preconditioner, -1.0);
-  //       v.toReal();
-  //       v*=maskCompl;
-  //       v.toFourier();
-  //       v.applyTransferFunction(preconditioner, 0.5);
-  //       v.toReal();
-  //       return v;
-  //     };
-
-
-  //     fields::Field<DataType,T> alpha = tools::numerics::conjugateGradient<DataType>(X,z);
-
-  //     alpha.toFourier();
-  //     alpha.applyTransferFunction(preconditioner, 0.5);
-  //     alpha.toReal();
-
-  //     fields::Field<DataType,T> bInDeltaBasis(b);
-  //     bInDeltaBasis.toFourier();
-  //     bInDeltaBasis.applyTransferFunction(preconditioner, 0.5);
-  //     bInDeltaBasis.toReal();
-
-  //     alpha*=maskCompl;
-  //     alpha+=bInDeltaBasis;
-
-  //     delta_diff*=mask;
-  //     alpha-=delta_diff;
-
-  //     assert(!alpha.isFourier());
-  //     alpha.toFourier();
-  //     alpha.applyTransferFunction(preconditioner, -0.5);
-  //     alpha.toReal();
-
-  //     return alpha;
-  // }
+  template<typename DataType, typename T=tools::datatypes::strip_complex<DataType>>
+  fields::OutputField<DataType> Mbar_Cm1_M(
+    const fields::OutputField<DataType> & inputs,
+    const auto& covs, const auto& masks, const auto& masksCompl, size_t Nlevel
+  ) {
+    auto outputs = inputs;
+    // outputs = T_op_T(outputs, [&](const int level, fields::Field<DataType,T> & input) {
+    for (size_t level = 0; level < Nlevel; ++level) {
+      auto & input = outputs.getFieldForLevel(level);
+      input.toFourier();
+      input.applyTransferFunction(covs[level], 0.5);
+      input.toReal();
+      input *= masks[level];
+      input.toFourier();
+      input.applyTransferFunction(covs[level], -1.0);
+      input.toReal();
+      input *= masksCompl[level];
+      input.toFourier();
+      input.applyTransferFunction(covs[level], 0.5);
+      input.toReal();
+    // });
+    }
+    outputs.toReal();
+    return outputs;
+  };
 
   template<typename DataType, typename T=tools::datatypes::strip_complex<DataType>>
   fields::OutputField<DataType> splice(fields::OutputField<DataType> & a,
@@ -150,50 +118,17 @@ namespace modifications {
       a.toFourier();
       b.toFourier();
 
-      fields::OutputField<T> delta_diff(b);
-      delta_diff -= a;
-      for (size_t level=0; level<Nlevel; ++level) {
-        auto & input = delta_diff.getFieldForLevel(level);
-        input.applyTransferFunction(covs[level], 0.5);
-        input.toReal();
-      }
+      fields::OutputField<T> delta(b);
+      delta -= a;
 
-      fields::OutputField<T> z(delta_diff);
-      for (size_t level=0; level<Nlevel; ++level) {
-        auto & z_level = z.getFieldForLevel(level);
-        z_level*=masks[level];
-        z_level.toFourier();
-        z_level.applyTransferFunction(covs[level], -1.0);
-        z_level.toReal();
-        z_level*=masksCompl[level];
-        z_level.toFourier();
-        z_level.applyTransferFunction(covs[level], 0.5);
-        z_level.toReal();
-      }
+      fields::OutputField<T> z = Mbar_Cm1_M(delta, covs, masks, masksCompl, Nlevel);
 
-      auto X = [&](const fields::OutputField<T> & inputs) -> fields::OutputField<T>
-      {
-        auto outputs = inputs;
-        for (size_t level=0; level<Nlevel; ++level) {
-          auto& v = outputs.getFieldForLevel(level);
-          assert (!v.isFourier());
-          v.toFourier();
-          v.applyTransferFunction(covs[level], 0.5);
-          v.toReal();
-          v*=masksCompl[level];
-          v.toFourier();
-          v.applyTransferFunction(covs[level], -1.0);
-          v.toReal();
-          v*=masksCompl[level];
-          v.toFourier();
-          v.applyTransferFunction(covs[level], 0.5);
-          v.toReal();
-        }
-        return outputs;
+      auto A = [&](const fields::OutputField<T> & inputs) {
+        return Mbar_Cm1_Mbar(inputs, covs, masks, masksCompl, Nlevel);
       };
 
 
-      fields::OutputField<DataType> alpha = tools::numerics::conjugateGradient<DataType>(X,z);
+      fields::OutputField<DataType> alpha = tools::numerics::conjugateGradient<DataType>(A, z);
 
       // alpha.toFourier();
       // alpha.applyTransferFunction(preconditioner, 0.5);
